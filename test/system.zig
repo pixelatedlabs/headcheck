ed7883650ddce653b4cf0aa395f40ac6e1e17eb0
Nathan Garside <git@nathangarside.com>
2025-08-31 19:11:53 +0100

    Simplify remaining tests

--- test/system.zig
+++ test/system.zig
@@ -15,12 +15,12 @@ pub fn main() !void {
     };
 
     try tooFewArguments(allocator, args);
-    try tooManyArguments();
-    try invalidUrl();
-    try validUrlWithSuccessfulResponse();
-    try validUrlWithUnsuccessfulResponse();
-    try helpText();
-    try versionText();
+    try tooManyArguments(allocator, args);
+    try invalidUrl(allocator, args);
+    try validUrlWithSuccessfulResponse(allocator, args);
+    try validUrlWithUnsuccessfulResponse(allocator, args);
+    try helpText(allocator, args);
+    try versionText(allocator, args);
 }
 
 fn tooFewArguments(allocator: std.mem.Allocator, args: Args) !void {
@@ -33,94 +33,63 @@ fn tooFewArguments(allocator: std.mem.Allocator, args: Args) !void {
     try std.testing.expectEqualStrings("usage: headcheck <url>\n", child.stdout);
 }
 
-fn tooManyArguments() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    const allocator = arena.allocator();
-    defer arena.deinit();
-
-    const binary = try std.process.getEnvVarOwned(allocator, "HEADCHECK_BINARY");
+fn tooManyArguments(allocator: std.mem.Allocator, args: Args) !void {
     const child = try std.process.Child.run(.{
         .allocator = allocator,
-        .argv = &[_][]const u8{ binary, "foo", "bar" },
+        .argv = &[_][]const u8{ args.binary, "foo", "bar" },
     });
 
     try std.testing.expectEqual(2, child.term.Exited);
     try std.testing.expectEqualStrings("usage: headcheck <url>\n", child.stdout);
 }
 
-fn invalidUrl() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    const allocator = arena.allocator();
-    defer arena.deinit();
-
-    const binary = try std.process.getEnvVarOwned(allocator, "HEADCHECK_BINARY");
+fn invalidUrl(allocator: std.mem.Allocator, args: Args) !void {
     const child = try std.process.Child.run(.{
         .allocator = allocator,
-        .argv = &[_][]const u8{ binary, "baz" },
+        .argv = &[_][]const u8{ args.binary, "baz" },
     });
 
     try std.testing.expectEqual(2, child.term.Exited);
     try std.testing.expectEqualStrings("unparseable: baz\n", child.stdout);
 }
 
-fn validUrlWithSuccessfulResponse() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    const allocator = arena.allocator();
-    defer arena.deinit();
-
-    const binary = try std.process.getEnvVarOwned(allocator, "HEADCHECK_BINARY");
+fn validUrlWithSuccessfulResponse(allocator: std.mem.Allocator, args: Args) !void {
     const child = try std.process.Child.run(.{
         .allocator = allocator,
-        .argv = &[_][]const u8{ binary, "http://www.google.com" },
+        .argv = &[_][]const u8{ args.binary, "http://www.google.com" },
     });
 
     try std.testing.expectEqual(0, child.term.Exited);
     try std.testing.expectEqualStrings("success: 200\n", child.stdout);
 }
 
-fn validUrlWithUnsuccessfulResponse() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    const allocator = arena.allocator();
-    defer arena.deinit();
-
-    const binary = try std.process.getEnvVarOwned(allocator, "HEADCHECK_BINARY");
+fn validUrlWithUnsuccessfulResponse(allocator: std.mem.Allocator, args: Args) !void {
     const child = try std.process.Child.run(.{
         .allocator = allocator,
-        .argv = &[_][]const u8{ binary, "http://google.com" },
+        .argv = &[_][]const u8{ args.binary, "http://google.com" },
     });
 
     try std.testing.expectEqual(1, child.term.Exited);
     try std.testing.expectEqualStrings("failure: 301\n", child.stdout);
 }
 
-fn helpText() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    const allocator = arena.allocator();
-    defer arena.deinit();
-
-    const binary = try std.process.getEnvVarOwned(allocator, "HEADCHECK_BINARY");
+fn helpText(allocator: std.mem.Allocator, args: Args) !void {
     const child = try std.process.Child.run(.{
         .allocator = allocator,
-        .argv = &[_][]const u8{ binary, "--help" },
+        .argv = &[_][]const u8{ args.binary, "--help" },
     });
 
     try std.testing.expectEqual(0, child.term.Exited);
     try std.testing.expectEqualStrings("docs: https://pixelatedlabs.com/headcheck\n", child.stdout);
 }
 
-fn versionText() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    const allocator = arena.allocator();
-    defer arena.deinit();
-
-    const binary = try std.process.getEnvVarOwned(allocator, "HEADCHECK_BINARY");
+fn versionText(allocator: std.mem.Allocator, args: Args) !void {
     const child = try std.process.Child.run(.{
         .allocator = allocator,
-        .argv = &[_][]const u8{ binary, "--version" },
+        .argv = &[_][]const u8{ args.binary, "--version" },
     });
 
-    const version = try std.process.getEnvVarOwned(allocator, "HEADCHECK_VERSION");
-    const output = try std.fmt.allocPrint(allocator, "version: {s}\n", .{version});
+    const output = try std.fmt.allocPrint(allocator, "version: {s}\n", .{args.version});
 
     try std.testing.expectEqual(0, child.term.Exited);
     try std.testing.expectEqualStrings(output, child.stdout);

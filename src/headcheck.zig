ceacdb9112734a0714eca0ce39c10db49469bec9
Nathan Garside <git@nathangarside.com>
2025-08-31 11:29:57 +0100

    Switch to arena allocator (#3)

--- src/headcheck.zig
+++ src/headcheck.zig
@@ -4,11 +4,12 @@ const config = @import("config");
 const std = @import("std");
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    const allocator = arena.allocator();
+    defer arena.deinit();
 
-    const args = try std.process.argsAlloc(gpa.allocator());
-    defer std.process.argsFree(gpa.allocator(), args);
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
 
     if (args.len != 2) {
         print("usage: headcheck <url>\n", .{});
@@ -25,7 +26,7 @@ pub fn main() !void {
         std.process.exit(0);
     }
 
-    var client = std.http.Client{ .allocator = gpa.allocator() };
+    var client = std.http.Client{ .allocator = allocator };
     defer client.deinit();
 
     const url = std.Uri.parse(args[1]) catch {

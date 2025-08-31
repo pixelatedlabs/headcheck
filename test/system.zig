f0e3d186f03f582911c8388d7f55f6f1825475ee
Nathan Garside <git@nathangarside.com>
2025-08-31 18:17:47 +0100

    Simplify running exe

--- test/system.zig
+++ test/system.zig
@@ -8,15 +8,11 @@ test "Too few arguments" {
     defer arena.deinit();
 
     const binary = try std.process.getEnvVarOwned(allocator, "HEADCHECK_BINARY");
-
-    const args = try std.process.argsAlloc(allocator);
-    defer std.process.argsFree(allocator, args);
-
-    const argv = [_][]const u8{binary};
     const child = try std.process.Child.run(.{
         .allocator = allocator,
-        .argv = &argv,
+        .argv = &[_][]const u8{binary},
     });
+
     try std.testing.expectEqualStrings("usage: headcheck <url>\n", child.stdout);
     try std.testing.expect(child.term.Exited == 2);
 }

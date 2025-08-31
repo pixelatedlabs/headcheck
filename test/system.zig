a752f8506418a907c24ae9bcf915dbbb1e8522fb
Nathan Garside <git@nathangarside.com>
2025-08-31 12:16:02 +0100

    Switch to zig test runner

--- test/system.zig
+++ test/system.zig
@@ -2,7 +2,7 @@
 
 const std = @import("std");
 
-pub fn main() !void {
+test "Too few arguments" {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     const allocator = arena.allocator();
     defer arena.deinit();
@@ -10,7 +10,8 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
-    const argv = [_][]const u8{args[1]};
+    // const argv = [_][]const u8{args[1]};
+    const argv = [_][]const u8{"/workspaces/Headcheck/zig-out/bin/headcheck"};
     var child = std.process.Child.init(&argv, allocator);
     try child.spawn();
     const exit = try child.wait();

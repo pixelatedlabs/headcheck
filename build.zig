23b832ee6f40ba554963b10131af15f59e4fecfc
Nathan Garside <git@nathangarside.com>
2025-09-03 13:31:17 +0100

    Sort options

--- build.zig
+++ build.zig
@@ -3,12 +3,13 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    // Add target option.
+    const target = b.standardTargetOptions(.{ .default_target = b.graph.host.query });
+
     // Add version option.
     const version = b.option([]const u8, "version", "version") orelse "0.0.0";
     const options = b.addOptions();
     options.addOption([]const u8, "version", version);
-
-    const target = b.standardTargetOptions(.{ .default_target = b.graph.host.query });
 
     // Build for arm64.
     crossBuild(b, options, version, target);

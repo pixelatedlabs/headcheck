26b325917fefa65509631020a9ccbc4dff035e46
Nathan Garside <git@nathangarside.com>
2025-09-03 12:55:42 +0100

    Lift options to root function

--- build.zig
+++ build.zig
@@ -3,20 +3,20 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    // Add version option.
+    const version = b.option([]const u8, "version", "version") orelse "0.0.0";
+    const options = b.addOptions();
+    options.addOption([]const u8, "version", version);
+
     // Build for x64.
-    crossBuild(b, b.resolveTargetQuery(.{
+    crossBuild(b, options, version, b.resolveTargetQuery(.{
         .cpu_arch = .x86_64,
         .cpu_model = .native,
         .os_tag = .linux,
     }));
 }
 
-fn crossBuild(b: *std.Build, target: std.Build.ResolvedTarget) void {
-    // Add version option.
-    const version = b.option([]const u8, "version", "version") orelse "0.0.0";
-    const options = b.addOptions();
-    options.addOption([]const u8, "version", version);
-
+fn crossBuild(b: *std.Build, options: *std.Build.Step.Options, version: []const u8, target: std.Build.ResolvedTarget) void {
     // Compile source code.
     const compile = b.addExecutable(.{
         .name = "headcheck",

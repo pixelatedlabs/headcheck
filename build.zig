d4b0b3bdba9c314bac670c4fb6d7acd284b3f671
Nathan Garside <git@nathangarside.com>
2025-09-03 13:29:12 +0100

    Add target flag

--- build.zig
+++ build.zig
@@ -8,19 +8,10 @@ pub fn build(b: *std.Build) void {
     const options = b.addOptions();
     options.addOption([]const u8, "version", version);
 
-    // Build for arm64.
-    crossBuild(b, options, version, b.resolveTargetQuery(.{
-        .cpu_arch = .aarch64,
-        .cpu_model = .baseline,
-        .os_tag = .linux,
-    }));
+    const target = b.standardTargetOptions(.{});
 
-    // Build for x64.
-    crossBuild(b, options, version, b.resolveTargetQuery(.{
-        .cpu_arch = .x86_64,
-        .cpu_model = .native,
-        .os_tag = .linux,
-    }));
+    // Build for arm64.
+    crossBuild(b, options, version, target);
 }
 
 fn crossBuild(b: *std.Build, options: *std.Build.Step.Options, version: []const u8, target: std.Build.ResolvedTarget) void {

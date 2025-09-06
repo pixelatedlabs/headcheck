04fb74ceadb8a59f173954cf757366983f95230e
Nathan Garside <git@nathangarside.com>
2025-09-06 10:53:10 +0100

    Remove old header script

--- doc/header.sh
+++ doc/header.sh
@@ -1,8 +1,20 @@
-#  _                    _      _               _
-# | |                  | |    | |             | |
-# | |__   ___  __ _  __| | ___| |__   ___  ___| | __
-# | '_ \ / _ \/ _` |/ _` |/ __| '_ \ / _ \/ __| |/ /
-# | | | |  __/ (_| | (_| | (__| | | |  __/ (__|   <
-# |_| |_|\___|\__,_|\__,_|\___|_| |_|\___|\___|_|\_\
+# This is free and unencumbered software released into the public domain.
 
-headcheck http://localhost/status
+DIR=$(dirname $BASH_SOURCE[0])
+
+read -r -d '' CODE <<-'EOF'
+	#  _                    _      _               _
+	# | |                  | |    | |             | |
+	# | |__   ___  __ _  __| | ___| |__   ___  ___| | __
+	# | '_ \ / _ \/ _` |/ _` |/ __| '_ \ / _ \/ __| |/ /
+	# | | | |  __/ (_| | (_| | (__| | | |  __/ (__|   <
+	# |_| |_|\___|\__,_|\__,_|\___|_| |_|\___|\___|_|\_\
+
+	headcheck http://localhost/status
+EOF
+
+echo "$CODE" | freeze --language fish --output $DIR/header.png --padding 20,200,0,30
+
+magick $DIR/header.png -resize 1270x $DIR/header.png
+
+optipng $DIR/header.png

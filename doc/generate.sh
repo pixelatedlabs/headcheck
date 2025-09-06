115404f01f507e5bcb85587a613c0fca5ea1b1e8
Nathan Garside <git@nathangarside.com>
2025-09-06 10:34:23 +0100

    Simplify generator

--- doc/generate.sh
+++ doc/generate.sh
@@ -1,9 +1,6 @@
 DIR=$(dirname $BASH_SOURCE[0])
 
-freeze $DIR/header.sh \
-	--language fish \
-	--output $DIR/header.png \
-	--padding 20,200,0,30
+freeze $DIR/header.sh --language fish --output $DIR/header.png --padding 20,200,0,30
 
 magick $DIR/header.png -resize 1270x $DIR/header.png
 

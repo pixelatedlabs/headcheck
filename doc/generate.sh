63bdee0efc11f224d0445cb2dad851da96547ea7
Nathan Garside <git@nathangarside.com>
2025-09-06 10:34:42 +0100

    Add missing license text

--- doc/generate.sh
+++ doc/generate.sh
@@ -1,3 +1,5 @@
+# This is free and unencumbered software released into the public domain.
+
 DIR=$(dirname $BASH_SOURCE[0])
 
 freeze $DIR/header.sh --language fish --output $DIR/header.png --padding 20,200,0,30

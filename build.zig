451b0f245608e005b2aaf70665357c9a69f8120d
Nathan Garside <git@nathangarside.com>
2025-09-08 19:37:50 +0100

    Base to single method

--- build.zig
+++ build.zig
@@ -3,144 +3,103 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const options, const version = generateOptions(b);
-    const compile_debug = compileDebug(b, options);
-    const compile_release = compileRelease(b, options);
-    const compress_upx = compressUpx(b, compile_release.getEmittedBin());
-    const compress_zip, const zip_output = compressZip(b, compile_release.getEmittedBin());
-    const test_system = testSystem(b, compile_release.getEmittedBin(), version);
-    const artifact_debug = b.addInstallArtifact(compile_debug, .{});
-    const name_full = nameFull(b, compile_release, version);
-    const name_platform = namePlatform(b, compile_release.rootModuleTarget());
-    const name_short = nameShort(b, compile_release);
-    const artifact_full = artifactFull(b, zip_output, name_full);
-    const artifact_short = artifactShort(b, zip_output, name_short);
-    const artifact_release = artifactInstall(b, compile_release, name_platform);
-    const artifact_run = artifactRun(b, compile_debug);
-
-    const option_install = b.getInstallStep();
-    option_install.dependOn(&artifact_debug.step);
-
-    const option_package = b.step("release", "Package for publishing");
-    compress_upx.step.dependOn(&compile_release.step);
-    test_system.step.dependOn(&compress_upx.step);
-    compress_zip.step.dependOn(&test_system.step);
-    artifact_release.step.dependOn(&compress_zip.step);
-    artifact_full.step.dependOn(&artifact_release.step);
-    artifact_short.step.dependOn(&artifact_full.step);
-    option_package.dependOn(&artifact_short.step);
-
-    const option_run = b.step("run", "Run the application");
-    option_run.dependOn(&artifact_run.step);
-}
-
-fn artifactFull(b: *std.Build, file: std.Build.LazyPath, name: []const u8) *std.Build.Step.InstallFile {
-    return b.addInstallFile(file, name);
-}
-
-fn artifactInstall(b: *std.Build, compile: *std.Build.Step.Compile, name: []const u8) *std.Build.Step.InstallArtifact {
-    return b.addInstallArtifact(compile, .{
-        .dest_dir = .{
-            .override = .{
-                .custom = name,
-            },
-        },
+    const target = b.standardTargetOptions(.{
+        .default_target = b.graph.host.query,
     });
-}
 
-fn artifactShort(b: *std.Build, file: std.Build.LazyPath, name: []const u8) *std.Build.Step.InstallFile {
-    return b.addInstallFile(file, name);
-}
+    const version = b.option([]const u8, "version", "version") orelse "0.0.0";
 
-fn artifactRun(b: *std.Build, compile: *std.Build.Step.Compile) *std.Build.Step.Run {
-    const command = b.addRunArtifact(compile);
-    if (b.args) |args| {
-        command.addArgs(args);
-    }
-    return command;
-}
+    const options = b.addOptions();
+    options.addOption([]const u8, "version", version);
 
-fn compileDebug(b: *std.Build, options: *std.Build.Step.Options) *std.Build.Step.Compile {
-    const compile = b.addExecutable(.{
+    const compile_debug_step = b.addExecutable(.{
         .name = "headcheck",
         .root_module = b.createModule(.{
             .optimize = .Debug,
             .root_source_file = b.path("src/main.zig"),
-            .target = b.graph.host,
+            .target = target,
         }),
     });
-    compile.root_module.addOptions("config", options);
-    return compile;
-}
+    compile_debug_step.root_module.addOptions("config", options);
 
-fn compileRelease(b: *std.Build, options: *std.Build.Step.Options) *std.Build.Step.Compile {
-    const compile = b.addExecutable(.{
+    const compile_release_step = b.addExecutable(.{
         .name = "headcheck",
         .root_module = b.createModule(.{
             .omit_frame_pointer = true,
             .optimize = .ReleaseSmall,
             .root_source_file = b.path("src/main.zig"),
             .single_threaded = true,
-            .target = b.standardTargetOptions(.{
-                .default_target = b.graph.host.query,
-            }),
+            .target = target,
             .unwind_tables = .none,
         }),
     });
-    compile.root_module.addOptions("config", options);
-    return compile;
-}
+    compile_release_step.root_module.addOptions("config", options);
+    const compile_release_output = compile_release_step.getEmittedBin();
 
-fn compressUpx(b: *std.Build, bin: std.Build.LazyPath) *std.Build.Step.Run {
-    const compress = b.addSystemCommand(&.{ "upx", "--lzma", "-9" });
-    compress.stdio = .{ .check = .{} };
-    compress.addFileArg(bin);
-    return compress;
-}
+    const upx_step = b.addSystemCommand(&.{ "upx", "--lzma", "-9" });
+    upx_step.stdio = .{ .check = .{} };
+    upx_step.addFileArg(compile_release_output);
+    upx_step.step.dependOn(&compile_release_step.step);
 
-fn compressZip(b: *std.Build, bin: std.Build.LazyPath) struct { *std.Build.Step.Run, std.Build.LazyPath } {
-    const command = b.addSystemCommand(&.{ "zip", "--junk-paths" });
-    const output = command.addOutputFileArg("headcheck.zip");
-    command.addFileArg(bin);
-    return .{ command, output };
-}
+    const test_step = b.addSystemCommand(&.{ "zig", "run", "test/system.zig", "--" });
+    test_step.addFileArg(compile_release_output);
+    test_step.addArg(version);
+    test_step.step.dependOn(&upx_step.step);
 
-fn generateOptions(b: *std.Build) struct { *std.Build.Step.Options, []const u8 } {
-    const options = b.addOptions();
-    const version = b.option([]const u8, "version", "version") orelse "0.0.0";
-    options.addOption([]const u8, "version", version);
-    return .{ options, version };
-}
+    const zip_step = b.addSystemCommand(&.{ "zip", "--junk-paths" });
+    const zip_output = zip_step.addOutputFileArg("headcheck.zip");
+    zip_step.addFileArg(compile_release_output);
+    zip_step.step.dependOn(&test_step.step);
 
-fn nameFull(b: *std.Build, compile: *std.Build.Step.Compile, version: []const u8) []const u8 {
-    return b.fmt("{s}_{s}_{s}.zip", .{
-        compile.name,
-        namePlatform(b, compile.rootModuleTarget()),
-        version,
-    });
-}
-
-fn namePlatform(b: *std.Build, target: std.Target) []u8 {
-    return b.fmt(
+    const name_platform = b.fmt(
         "{s}_{s}",
         .{
-            @tagName(target.os.tag),
-            switch (target.cpu.arch) {
+            @tagName(target.result.os.tag),
+            switch (target.result.cpu.arch) {
                 .aarch64 => "arm64",
                 .x86_64 => "x64",
                 else => |a| @tagName(a),
             },
         },
     );
-}
 
-fn nameShort(b: *std.Build, compile: *std.Build.Step.Compile) []const u8 {
-    return b.fmt("{s}.zip", .{namePlatform(b, compile.rootModuleTarget())});
-}
+    const name_full = b.fmt("{s}_{s}_{s}.zip", .{
+        compile_release_step.name,
+        name_platform,
+        version,
+    });
 
-fn testSystem(b: *std.Build, bin: std.Build.LazyPath, version: []const u8) *std.Build.Step.Run {
-    const testing = b.addSystemCommand(&.{ "zig", "run", "test/system.zig", "--" });
-    testing.addFileArg(bin);
-    testing.addArg(version);
-    return testing;
+    const name_short = b.fmt("{s}.zip", .{name_platform});
+
+    const artifact_exe_debug_step = b.addInstallArtifact(compile_debug_step, .{});
+
+    const artifact_exe_release_step = b.addInstallArtifact(compile_release_step, .{
+        .dest_dir = .{
+            .override = .{
+                .custom = name_platform,
+            },
+        },
+    });
+    artifact_exe_release_step.step.dependOn(&zip_step.step);
+
+    const artifact_zip_full_step = b.addInstallFile(zip_output, name_full);
+    artifact_zip_full_step.step.dependOn(&artifact_exe_release_step.step);
+
+    const artifact_zip_short_step = b.addInstallFile(zip_output, name_short);
+    artifact_zip_short_step.step.dependOn(&artifact_zip_full_step.step);
+
+    const run_step = b.addRunArtifact(compile_debug_step);
+    if (b.args) |args| {
+        run_step.addArgs(args);
+    }
+    run_step.step.dependOn(&compile_debug_step.step);
+
+    const option_install = b.getInstallStep();
+    option_install.dependOn(&artifact_exe_debug_step.step);
+
+    const option_package = b.step("release", "Package for publishing");
+    option_package.dependOn(&artifact_zip_short_step.step);
+
+    const option_run = b.step("run", "Run the application");
+    option_run.dependOn(&run_step.step);
 }

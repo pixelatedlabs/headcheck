54456673b2329653be5a6d56d5dbc2095df88043
Nathan Garside <git@nathangarside.com>
2026-04-23 17:00:43 +0100

    Get allocator from main

--- src/main.zig
+++ src/main.zig
@@ -3,10 +3,8 @@
 const config = @import("config");
 const std = @import("std");
 
-pub fn main() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    const allocator = arena.allocator();
-    defer arena.deinit();
+pub fn main(init: std.process.Init) !void {
+    const allocator = init.arena.allocator();
 
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
